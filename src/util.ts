d4e79dd1d105d0e431508f314973d9a1583a9b28
Tamino Martinius <dev@zaku.eu>
2018-09-25 18:35:51 +0200

    Remove applyMixin function

--- src/util.ts
+++ src/util.ts
@@ -1,11 +1,3 @@
-export function applyMixins(derivedCtor: any, baseCtors: any[]) {
-  baseCtors.forEach(baseCtor => {
-    Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
-      derivedCtor.prototype[name] = baseCtor.prototype[name];
-    });
-  });
-};
-
 export function staticImplements<T>() {
   return (_constructor: T) => {
   };
