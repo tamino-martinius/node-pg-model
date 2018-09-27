7e88f76d3227d26104289c999ce38312290abb73
Tamino Martinius <dev@zaku.eu>
2018-09-27 22:02:18 +0200

    Add string conversion

--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,11 @@ export function staticImplements<T>() {
   return (_: T) => {
   };
 }
+
+export function snakeToCamelCase(value: string) {
+  return value.replace(/_\w/g, m => m[1].toUpperCase());
+}
+
+export function camelToSnakeCase(value: string) {
+  return value.replace(/([A-Z])/g, m => `_${m.toLowerCase()}`);
+}
