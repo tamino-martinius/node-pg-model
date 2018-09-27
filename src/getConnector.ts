0d4cffa97053e39a3ca3865b08a5f04087cd5cc7
Tamino Martinius <dev@zaku.eu>
2018-09-27 22:01:00 +0200

    Add greater than equals filter

--- src/getConnector.ts
+++ src/getConnector.ts
@@ -142,6 +142,19 @@ async function gtFilter<S extends Schema>(values: any[], filters: Partial<S>) {
   return query;
 }
 
+async function gteFilter<S extends Schema>(values: any[], filters: Partial<S>) {
+  const queryParts: string[] = [];
+  let query = '(1 = 1)';
+  if (Object.keys(filters).length > 0) {
+    for (const column in filters) {
+      values.push(filters[column]);
+      queryParts.push(`("${column}" >= $${values.length})`);
+    }
+    query = queryParts.join(' AND ');
+  }
+  return query;
+}
+
   if (Object.keys(filter).length !== 1) throw '[TODO] Return proper error';
   if (filter.$and !== undefined)
     return await andFilter(values, filter.$and);
