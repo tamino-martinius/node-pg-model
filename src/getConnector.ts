8fb359c323dc832fc6c13ea44614404b70e1e60a
Tamino Martinius <dev@zaku.eu>
2018-09-29 17:56:00 +0200

    Implement connector select function

--- src/getConnector.ts
+++ src/getConnector.ts
@@ -330,8 +330,17 @@ ${getOffset(model)}
       const { rows } = await model.pool.query(queryText, values);
       return rows[0].count;
     },
-    select(model: ModelStatic<S>, keys: (keyof S)[]): Promise<S[keyof S][][]> {
-      throw 'not yet implemented';
+    async select(model: ModelStatic<S>, columns: string[]): Promise<any[]> {
+      const values: any[] = [];
+      const queryText = `
+${getSelect(model, columns)}
+${getFrom(model)}
+${getWhere(model, values)}
+${getLimit(model)}
+${getOffset(model)}
+`;
+      const { rows } = await model.pool.query(queryText, values);
+      return rows;
     },
     updateAll(model: ModelStatic<S>, attrs: Partial<S>): Promise<number> {
       throw 'not yet implemented';
